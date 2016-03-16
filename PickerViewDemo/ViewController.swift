8c519e5576f548beb6b5a616b37ac071d85d8540
Greg Willis <gregorymwillis@gmail.com>
2016-03-16 13:57:11 -0600

    Figured out how to use a PickerView as the inputView for a TextField.

--- PickerViewDemo/ViewController.swift
+++ PickerViewDemo/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var sportTextField: UITextField!
+    @IBOutlet var skillLevelTextField: UITextField!
+    var pickerView: UIPickerView!
+    
+    var sportData = ["Skiing", "Snowboarding", "Option One", "Option Two"]
+    var skillData = ["Beginner", "Intermediate", "Advanced", "Expert"]
+    
+    var selections = [UITextField: [String]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pickerView = UIPickerView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        sportTextField.inputView = pickerView
+        sportTextField.text = "Select"
+        skillLevelTextField.inputView = pickerView
+        skillLevelTextField.text = "Select"
+        
+        pickerView.removeFromSuperview()
+        
+        selections[sportTextField] = sportData
+        selections[skillLevelTextField] = skillData
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,60 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func getText() -> Int {
+        return pickerView.selectedRowInComponent(0)
+    }
 }
 
+
+extension ViewController: UIPickerViewDataSource {
+    
+    func numberOfComponentsInPickerView(colorPicker: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sportData.count
+    }
+    
+    
+}
+
+extension ViewController: UIPickerViewDelegate {
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        if sportTextField.isFirstResponder() {
+//            return sportData[row]
+//        }
+//        if skillLevelTextField.isFirstResponder() {
+//            return skillData[row]
+//        }
+        
+        var result: String? = nil
+        
+        selections.forEach { (text, data) in
+            if text.isFirstResponder() {
+                result = data[row]
+            }
+        }
+        
+        return result
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        if sportTextField.isFirstResponder() {
+//            sportTextField.text = "\(sportData[getText()])"
+//            sportTextField.resignFirstResponder()
+//        }
+//        if skillLevelTextField.isFirstResponder() {
+//            skillLevelTextField.text = "\(skillData[getText()])"
+//            skillLevelTextField.resignFirstResponder()
+//        }
+        selections.forEach { (text, data) -> () in
+            if text.isFirstResponder() {
+                text.text = data[getText()]
+                text.resignFirstResponder()
+            }
+        }
+    }
+}
